a34f26e6d8618572f57d8cada278c4f2d6f1908b
Arman Akhtar <armanakhtar297@gmail.com>
2026-02-17 19:03:15 +0530

    navbar-1.1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuBar = document.querySelector(".menubar");
-const navheight=document.querySelector("#main-container-height");
 const list= document.querySelector(".nav-links ul");
 const icon = menuBar.querySelector("i");
+const navLinks = document.querySelectorAll(".nav-links ul li a");
 
 menuBar.addEventListener("click", () => {
     icon.classList.toggle("fa-bars");
@@ -9,11 +9,19 @@ menuBar.addEventListener("click", () => {
 
     if (window.getComputedStyle(list).display === "none") {
         list.style.display = "flex";
-        navheight.style.height = "100vh";
 
     } else {
         list.style.display = "none";
-        navheight.style.height = "80px";
 
     }
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        if (window.innerWidth <= 992) {
+            list.style.display = "none";
+            icon.classList.remove("fa-x");
+            icon.classList.add("fa-bars");
+        }
+    });
 });
